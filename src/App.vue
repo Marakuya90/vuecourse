daab05a7a63373714597d4952c8cc1bd13111f97
Marakuya90 <zhilzhil@mail.ru>
2023-06-11 11:36:04 +0300

    component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Актуальные новости {{ now }}</h1>
+  <span>Открыто:{{ openRate }} </span>
+<app-news
+    v-for="item in news"
+    :title="item.title"
+    :key="item.id"
+    :id="item.id"
+    :is-open="item.isOpen"
+    @open-news=openNews
+></app-news>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import AppNews from "@/components/AppNews.vue";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {AppNews},
+  data () {
+    return {
+      now: new Date().toLocaleDateString(),
+      openRate: 0,
+      news: [
+        {
+          title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, saepe',
+          id: 1,
+          isOpen: false
+        },
+        {
+          title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, doloremque temporibus?',
+          id: 2,
+          isOpen: false
+        },
+        {
+          title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis pariatur perferendis suscipit voluptate?',
+          id: 3,
+          isOpen: false
+        }
+      ]
+    }
+  },
+  methods: {
+    openNews() {
+      this.openRate++
+    }
   }
+
 }
 </script>
 
---
+++ src/components/AppNews.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="card container mb-4">
+  <h3 class="pt-2">{{ title }}</h3>
+  <button @click="openNews" class="btn btn-success mb-2" >{{ isNewOpen ? "Закрыть новость" : "Открыть новость" }}</button>
+  <p v-if="isNewOpen">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere fugit ipsum iure libero magnam modi molestiae nam quae quibusdam sapiente. Ab aliquid consequuntur culpa deleniti dolores enim expedita iure laborum libero minus modi necessitatibus perspiciatis quaerat quibusdam saepe vitae, voluptatem!</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: "AppNews.vue",
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    id: {
+      type: Number,
+      required: true
+    },
+    isOpen: {
+      type: Boolean,
+      required: false,
+      default: false,
+      validator(value) {
+        return value == true || value == false
+      }
+    }
+  },
+  data() {
+    return {
+      isNewOpen: this.isOpen
+    }
+  },
+  methods: {
+    openNews() {
+      this.isNewOpen = !this.isNewOpen
+      if(this.isNewOpen) {
+        this.$emit('open-news')
+      }
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  width: 220px;
+  margin: 0 auto;
+}
+</style>
