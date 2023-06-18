5546a00f09ce1d0b2226cb53de00c08095f67f90
Marakuya90 <zhilzhil@mail.ru>
2023-06-18 09:03:16 +0300

    axios,remove,mounted

--- formhttp/src/App.vue
+++ formhttp/src/App.vue
@@ -1,26 +1,99 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-5 pt-5 pb-5 ps-4 pe-4">
+    <h2>Работа с базой данных</h2>
+    <hr/>
+    <form @submit.prevent="createPerson">
+      <div>
+        <label class="form-label"
+            for="name">Введите имя</label>
+        <input class="form-control" type="text" id="name" v-model.trim="name">
+      </div>
+      <button class="btn btn-primary mt-4"
+      :disabled="name.length === 0"
+      >Cоздать</button>
+    </form>
+    <hr/>
+<app-people-list
+    :people="people"
+    @load="listPeopleLoader"
+    @delete="deletePerson"
+>
+</app-people-list>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppPeopleList from "@/components/AppPeopleList.vue";
+import axios from "axios";
+// import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      name: '',
+      people: []
+    }
+  },
+  mounted() {
+    this.listPeopleLoader()
+  },
+  methods: {
+    async createPerson() {
+      const response = await fetch('https://vue-with-http-7e764-default-rtdb.europe-west1.firebasedatabase.app/people.json',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type':'application/json'},
+            body: JSON.stringify({
+              firstName: this.name,
+            })
+          })
+
+      const firebaseData = await response.json()
+      this.people.push({
+        firstName: this.name,
+        id:firebaseData.name
+      })
+      this.name = ''
+    },
+
+    //запрос с помощью axios
+    // async listPeopleLoader() {
+    //   await axios.get('https://vue-with-http-7e764-default-rtdb.europe-west1.firebasedatabase.app/people.json')
+    // },
+
+    async listPeopleLoader() {
+      try {
+        const response = await fetch('https://vue-with-http-7e764-default-rtdb.europe-west1.firebasedatabase.app/people.json', {
+          method:'GET'
+        })
+        const listPeople = await response.json()
+        console.log(listPeople)
+        this.people = Object.keys(listPeople).map(key => {
+          return {
+            id: key,
+            //...listPeople[key]
+            firstName: listPeople[key].firstName
+          }
+        })
+      } catch (e) {
+        console.log(e.message)
+      }
+    },
+    async deletePerson(id) {
+      await axios.delete(`https://vue-with-http-7e764-default-rtdb.europe-west1.firebasedatabase.app/people/${id}.json`)
+    console.log(id)
+      this.people = this.people.filter(person => person.id !== id)
+    }
+  },
+  components: { AppPeopleList }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+div {
+  border-radius: 10px;
+  background-color: #FFFFFF;
+
 }
 </style>
---
+++ formhttp/src/components/AppPeopleList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="people.length !== 0">
+  <div
+      class="card mb-2 d-flex flex-row justify-content-between"
+       v-for="person in people"
+       :key="person.id">
+    <h2 class="ps-3">{{ person.firstName }}</h2>
+    <button
+        @click="$emit('delete',person.id)"
+        class="btn btn-outline-danger btn-sm">Удалить</button>
+  </div>
+  </div>
+  <div v-else>
+    <h4>Список пуст</h4>
+    <button @click="$emit('load')"
+            class="btn btn-outline-primary mt-3">Загрузить список</button>
+  </div>
+</template>
+
+<script>
+export default {
+  emits:['load','delete'],
+  name: "AppPeopleList.vue",
+  props:['people']
+}
+</script>
+
+<style scoped>
+
+</style>
